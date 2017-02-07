e021f7443cf2b63dfe08851750cf485b2b1fbd0b
nurgül <nurguldemir@anadolu.edu.tr>
2017-02-07 16:08:01 +0300

    topicler arasi iletisim

---
+++ deneme1/ESP8266_UserCongiguration.h
@@ -0,0 +1,14 @@
+/*
+ * ESP8266_UserCongiguration.h
+ *
+ *  Created on: 05 Feb 2017
+ *      Author: Orkan Murat CELIK
+ */
+#ifndef APPLICATION_ESP8266_USERCONFIGUTATION_H_
+#define APPLICATION_ESP8266_USERCONFIGUTATION_H_
+
+
+#define wifi_ssid "G4_1299"
+#define wifi_password "nunununu"
+
+#endif /* APPLICATION_ESP8266_USERCONFIGUTATION_H_ */
